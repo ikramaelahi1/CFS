419bdbab13714707d25a0b29530a5e8754a3e608
ikramaelahi1 <ikramaelahi1@gmail.com>
2019-06-15 20:18:35 +0100

    cfs

--- app/src/main/java/com/example/cfs/menu.java
+++ app/src/main/java/com/example/cfs/menu.java
@@ -6,16 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class menu extends AppCompatActivity {
-
+private Button fries,brgr,pizza,drink;
     Button log;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        pizza = (Button)findViewById(R.id.pizzabtn);
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),pizza.class);
+                startActivity(intent);
+            }
+        });
 
         log = (Button)findViewById(R.id.logout);
         log.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +34,7 @@ public class menu extends AppCompatActivity {
                 startActivity(back);
                 finish();
                 FirebaseAuth.getInstance().signOut();
+                Toast.makeText(menu.this, "SignOut Successfull", Toast.LENGTH_SHORT).show();
             }
 
         });
---
+++ app/src/main/java/com/example/cfs/pizza.java
@@ -0,0 +1,13 @@
+package com.example.cfs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pizza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza);
+    }
+}
